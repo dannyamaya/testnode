ef4a7e2e67c4848759233280f9e934f729b0e3d0
danny <danny@cannedhead.com>
2017-06-08 19:18:02 -0500

    filesss

--- models/note.js
+++ models/note.js
@@ -5,6 +5,8 @@ var NotesSchema   = new Schema({
   discussion_id:{ type: Schema.Types.ObjectId, ref: 'Ticket', required:true },
   posted_by: { type: Schema.Types.ObjectId, ref: 'User', required:true },
   note: { type: String, required: true},
+  attachments: { type: String, default:'' },
+  file_name: { type: String, default:'' },
   created: { type: Date, default: Date.now },
   updated: { type: Date, default: Date.now }
 });

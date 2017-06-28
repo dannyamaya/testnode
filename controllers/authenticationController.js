46d43d566d24d9c8a57fe57164f421592b6287ed
danny <danny@cannedhead.com>
2017-06-28 12:30:21 -0500

    ajuste

--- controllers/authenticationController.js
+++ controllers/authenticationController.js
@@ -16,20 +16,24 @@ module.exports = {
     loginUser: function (req, res, next) {
 
         if (req.body.email === undefined || req.body.email === "") {
-            return res.status(404).json({message: "Por favor ingrese un Email"});
+            return res.status(404).json({message: "Please enter an Email"});
         }
         if (req.body.password === undefined || req.body.password === "") {
-            return res.status(404).json({message: "Por favor ingrese una Contraseña"});
+            return res.status(404).json({message: "Please enter a password"});
         }
 
         passport.authenticate('local', function (err, user, message) {
 
             if (err) {
-                return res.status(500).json({message: 'Ha sucedido un error. Por favor intentelo nuevamente!'});
+                return res.status(500).json({message: 'Something is wrong try later.'});
             }
 
             if (!user) {
                 return res.status(400).json({message: message.message});
+            }
+
+            if(user.active == 'false'){
+                return res.status(400).json({message: 'User not found'});
             }
 
             req.login(user, {}, function (err) {
